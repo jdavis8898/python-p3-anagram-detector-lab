27a319353a283d24585cd989ce5ffc946ebbcf57
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-11 20:16:54 -0600

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+import ipdb
+
+class Anagram():
+
+    def __init__(self, word):
+
+        self.word = word
+    
+    def match(self, checklist):
+        
+        anagram_list = []
+        matching_word = [letter for letter in self.word]
+
+        for word in checklist:
+            if (sorted(matching_word) == (sorted([letter for letter in word]))):
+                anagram_list.append(word)
+
+        return anagram_list
